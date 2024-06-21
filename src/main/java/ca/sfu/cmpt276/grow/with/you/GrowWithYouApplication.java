adca28c39a5c7553053b9ad64304b1c975ed38ac
unknown <github.com.uo5n8@aleeas.com>
2024-06-21 15:44:51 -0700

    initial setup

---
+++ src/main/java/ca/sfu/cmpt276/grow/with/you/GrowWithYouApplication.java
@@ -0,0 +1,13 @@
+package ca.sfu.cmpt276.grow.with.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowWithYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowWithYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sfu/cmpt276/grow/with/you/GrowWithYouApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sfu.cmpt276.grow.with.you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowWithYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
