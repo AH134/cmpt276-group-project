9ebc503615409239f0635d73b05346715168447b
Devin <devindang7@gmail.com>
2024-06-26 10:14:26 -0700

    register mappings

---
+++ src/main/java/ca/sfu/cmpt276/grow/with/you/models/User.java
@@ -0,0 +1,72 @@
+package ca.sfu.cmpt276.grow.with.you.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+    private String username;
+    private String password;
+    private String email;
+    private double balance;
+    private boolean isAdmin;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, double balance, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.balance = balance;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+}
