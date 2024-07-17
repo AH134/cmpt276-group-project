e67e9947f652e3097502678b12e8436370f340a6
Devin Dang <devindang7@gmail.com>
2024-07-16 17:27:05 -0700

    edit username

--- src/main/java/ca/sfu/cmpt276/grow/with/you/controllers/ProfileController.java
+++ src/main/java/ca/sfu/cmpt276/grow/with/you/controllers/ProfileController.java
@@ -6,13 +6,20 @@ import ca.sfu.cmpt276.grow.with.you.models.User;
 import ca.sfu.cmpt276.grow.with.you.services.PlantService;
 import ca.sfu.cmpt276.grow.with.you.services.UserService;
 import ca.sfu.cmpt276.utils.setHttpHeader;
+import ca.sfu.cmpt276.utils.enums.UserError;
 import ca.sfu.cmpt276.utils.enums.UserRole;
 
 import org.springframework.ui.Model;
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
@@ -49,4 +56,43 @@ public class ProfileController {
         return "protected/user/profile";
     }
 
+    @GetMapping("/profile/edit")
+    public String getEditProfile(Model model, HttpSession session, HttpServletResponse res) {
+        User user = userService.getUserBySession(session);
+        setHttpHeader.setHeader(res);
+
+        if (user == null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", user);
+        return "protected/user/edit";
+    }
+
+    @PostMapping("/profile/edit")
+    public String editProfile(@RequestParam Map<String, String> formInput, HttpSession session, HttpServletResponse res,
+            HttpServletRequest req, Model model){
+        try{
+            setHttpHeader.setHeader(res);
+            User user = (User) session.getAttribute("session_user");
+            String newName = formInput.get("username");
+            User userByName = userService.getUserByUsername(newName);
+            UserError nameError = UserError.USERNAME_TAKEN;
+
+            if (userByName != null) {
+                model.addAttribute("user", formInput);
+                model.addAttribute("error1", nameError.getMessage());
+                return "protected/user/edit";
+            }
+            else{
+                user.setUsername(newName);
+                userService.createUser(user);
+                return "redirect:/profile";
+            }
+        }
+        catch(Exception e){
+            res.setStatus(400);
+            return "protected/user/profile";
+        }
+    }
 }
