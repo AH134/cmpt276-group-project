a67c2ed95ac7d8ba91af9c2ebef3efd58c1878ea
AH134 <github.com.uo5n8@aleeas.com>
2024-07-30 23:55:57 -0700

    Payment dashboard

--- src/main/java/ca/sfu/cmpt276/grow/with/you/models/PaymentRepository.java
+++ src/main/java/ca/sfu/cmpt276/grow/with/you/models/PaymentRepository.java
@@ -1,7 +1,11 @@
 package ca.sfu.cmpt276.grow.with.you.models;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    List<Payment> findByGrower(Grower grower, Sort sort);
 
+    List<Payment> findBySponsor(Sponsor sponsor, Sort sort);
 }
